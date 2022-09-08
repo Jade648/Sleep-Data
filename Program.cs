dd7b8f75b258783879f69050a27aeace88f74100
Jade645 <maddiepeterson375@gmail.com>
2022-09-07 19:58:54 -0500

    updated program and fixed code

--- Program.cs
+++ Program.cs
@@ -11,53 +11,54 @@ namespace SleepData
         static void Main(string[] args)
         {
 
-            // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
-            // input response
+            
             string resp = Console.ReadLine();
 
             if (resp == "1")
             {
 
-                 // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
-                // input the response (convert to int)
+                
                 int weeks = int.Parse(Console.ReadLine());
 
                 DateTime today = DateTime.Now;
-                // we want full weeks sunday - saturday
+               
                 DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-                // subtract # of weeks from endDate to get startDate
+                
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
 
                 Console.WriteLine(dataDate);
-
-                Random rnd = new Random();
-
-                while (dataDate < dataEndDate)
-                {
-                    // 7 days in a week
-                    int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
-                    {
-                        // generate random number of hours slept between 4-12 (inclusive)
-                        hours[i] = rnd.Next(4, 13);
-                    }
-                    // M/d/yyyy,#|#|#|#|#|#|#
+                    
                     Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    // add 1 week to date
+                   
                     dataDate = dataDate.AddDays(7);  
 
- StreamWriter sw = new StreamWriter("data.txt");
-                   
-            sw.WriteLine ($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                }
-                
-              sw.Close();
+                StreamWriter sw = new StreamWriter("data.txt");
 
-            } else if (resp == "2")
+                  Random rnd = new Random();
+ 
+                while (dataDate < dataEndDate){
+
+                int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++)
+                    {
+                        hours[i] = rnd.Next(4, 13);
+                    }
+                    
+                    Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                
+                    dataDate = dataDate.AddDays(7);
+                }
+
+                sw.Close();
+            }
+
+            else if (resp == "2")
             
             {
                 // TODO: parse data file
